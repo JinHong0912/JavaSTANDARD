486bbca8764b1713af552648b21dc58db70871a2
JinHong Ryu <jin.ryu0912@gmail.com>
2021-03-30 00:00:25 +0900

    [실습]변수_연습문제

---
+++ javaStandard/src/javaStandard/Ex2Test.java
@@ -0,0 +1,29 @@
+package javaStandard;
+
+public class Ex2Test {
+
+	
+		public static void main(String[] args) {
+		//2장 연습문제
+		//[2-5]
+		System.out.println("1"+"2");
+	  	
+		System.out.println(true+ "");
+		
+		System.out.println('A'+'B');
+		
+		System.out.println('1');//
+		System.out.println('1'+2);//
+		System.out.println('1'+'2');//
+		
+		System.out.println('J'+"ava");
+		
+		//System.out.println(true+null);// null은 출력 할 수 없음
+			
+			
+			
+		}
+		
+		
+}
+
