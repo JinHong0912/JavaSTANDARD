15d7f2881437dbd9ce8ed4cd98683e980f690dea
JinHong Ryu <jin.ryu0912@gmail.com>
2021-04-11 22:17:36 +0900

    [실습]자바의정석 : 배열

--- javaStandard/src/javaStandard/JavaEx3_9.java
+++ javaStandard/src/javaStandard/JavaEx3_9.java
@@ -6,16 +6,30 @@ public class JavaEx3_9 {
 
 	public static void main(String[] args) throws IOException {
 
-	int input = 0;
-	
-	System.out.println("문장을 입력하세요");
-	System.out.println("입략을 마치면 x을 입력하세요");
-	
-	
-	do {
-		input = System.in.read();
-		System.out.println((char)input);
-	}while(input != -1 && input !='x');		
+		//배열
+		//배열 정렬 하기 
+		int[] number = new int[10];
+		
+		for(int i = 0; i <number.length; i++ ) {
+			number[i] = i; // 배열을 0~ 9 의 숫자로 초기화 한다,
+			
+			System.out.print(number[i]);
+		}
+		System.out.println();
+		
+		for(int i =0; i < 100; i++) {
+			int n = (int)(Math.random() * 10 ); // 0부 터 9까지의 의 값을 얻는다.
+			int temp = number[0];//1
+			number[0] = number[n];
+			number[n] = temp;  //number[0]과 number[n] 의 값을 서로 바꾼다.temp에 하나의 값을 넣는다.
+			
+		}
+		
+		for(int i = 0; i < number.length ; i++) {
+			System.out.print(number[i]); // 배열의 내용을 출력 한다
+		}
+		
+		
 		
 	} 
 }
---
+++ javaStandard/src/javaStandard/JavaEx5_2.java
@@ -0,0 +1,21 @@
+package javaStandard;
+
+import java.io.IOException;
+
+public class JavaEx5_2 {
+
+	public static void main(String[] args) throws IOException {
+
+	int input = 0;
+	
+	System.out.println("문장을 입력하세요");
+	System.out.println("입략을 마치면 x을 입력하세요");
+	
+	
+	do {
+		input = System.in.read();
+		System.out.println((char)input);
+	}while(input != -1 && input !='x');		
+		
+	} 
+}
