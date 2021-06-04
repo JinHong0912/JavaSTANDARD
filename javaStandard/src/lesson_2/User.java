d86ae18d47cd6f9654f2a2a69012014b8f366eba
JinHong Ryu <jin.ryu0912@gmail.com>
2021-06-05 00:06:25 +0900

    [실습] Lesson2

---
+++ javaStandard/src/lesson_2/User.java
@@ -0,0 +1,23 @@
+package lesson_2;
+
+public class User {
+	
+	public static void main(String[] args) {
+		
+		// 전부 프린터를 사용해서 출력해 보세요
+		
+		Printer pt_1 = new Printer();
+		
+		pt_1.powerOn();
+		pt_1.print("출력테스트1");
+		pt_1.print("출력테스트2");
+		pt_1.print("출력테스트3");
+		pt_1.print("출력테스트4");
+		pt_1.print("출력테스트5");
+	
+	
+	
+	
+	}
+
+}
